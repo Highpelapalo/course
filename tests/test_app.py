f99772690f7b555ed7fe7a58d9da3e081a998aad
user <you@eample.com>
2017-01-08 18:59:59 +0200

    Added the system test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,21 @@
 import pytest
+import time
 import requests
+import foobar
 from multiprocessing import Process
 
+def set_up():
+    foobar.flapp.run()
 @pytest.fixture
-def flapp(request):
-    def set_up():
-        flapp.run()
+def flask_obj(request):
     p = Process(target=set_up)
     p.start()
-
+    time.sleep(3)
     def kill_it():
-        p.shutdown()
+        p.terminate()
     request.addfinalizer(kill_it)
 
 
-def test_app(flapp):
-    request = requests.get('http://localost:5000/bar')
+def test_app(flask_obj):
+    request = requests.get('http://localhost:5000/bar')
     assert request.status_code == 200
