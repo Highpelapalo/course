7641b6aba9ba428d72de0aa5dcb26ad587f1c079
user <you@eample.com>
2017-01-09 00:55:41 +0200

    Added full travis docker support

--- tests/test_running.py
+++ tests/test_running.py
@@ -1,8 +1,8 @@
 import foobar, requests
 
 def test_running():
-    foo_recv = requests.get("localhost:80/foo")
+    foo_recv = requests.get("localhost:5000/foo")
     assert foo_recv.status_code == 200
 
-    bar_recv = requests.get("localhost:80/bar")
+    bar_recv = requests.get("localhost:5000/bar")
     assert bar_recv.status_code == 200
