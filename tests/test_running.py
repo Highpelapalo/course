6178209274fe358de1b21191f215d9580cbd0113
user <you@eample.com>
2017-01-09 09:56:29 +0200

    Fixed test_running test

--- tests/test_running.py
+++ tests/test_running.py
@@ -1,8 +1,8 @@
 import foobar, requests
 
 def test_running():
-    foo_recv = requests.get("localhost:5000/foo")
+    foo_recv = requests.get("http://127.0.0.1/foo")
     assert foo_recv.status_code == 200
 
-    bar_recv = requests.get("localhost:5000/bar")
+    bar_recv = requests.get("http://127.0.0.1/bar")
     assert bar_recv.status_code == 200
